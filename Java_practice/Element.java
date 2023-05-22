543462cb1ab3fea0c74e9910bdf748ae0c40d668
aya-shiro <whitecheck00@gmail.com>
2023-05-22 11:24:15 +0000

    [update]practice

--- Java_practice/Element.java
+++ Java_practice/Element.java
@@ -1,12 +1,21 @@
-  class Element {
-    public static int count = 0;
-    public int createId;
-    public String elementName;
-    public String about;
-    public double height;
-    public double width;
+class Element {
+  public int count = 0;
+  public int createId;
+  public String elementName;
+  public String about;
+  public double height;
+  public double width;
 
-    public void printData() {
-      System.out.println("これは" + createId + "つ目の" + elementName + "です");
-    }
+  Element(int createId, String elementName,
+  String abount, double height, double width) {
+    this.createId = createId;
+    this.elementName = elementName;
+    this.about = about;
+    this.height = height;
+    this.width = width;
   }
+
+  public void printData() {
+    System.out.println("これは" + createId + "つ目の" + elementName + "です");
+  }
+}
