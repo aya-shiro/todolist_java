1f6d41f2891f06ecca26764d795953f899a3ee6d
aya-shiro <whitecheck00@gmail.com>
2023-05-24 11:24:55 +0000

    [practice]

--- Java_practice/Main.java
+++ Java_practice/Main.java
@@ -1,7 +1,26 @@
 class Main {
   public static void main(String[] args) {
-    Element element = new Element(1, "Railsmachine", "about_rails", 20.0, 15.0);
-    System.out.println(element.createId);
-    element.printData();
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+    Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0);
+
+    Car car = new Car("フェラーリ", "赤");
+    Bicycle bicycle = new Bicycle("ビアンキ", "緑");
+
+    person1.buy(car);
+
+    person2.buy(bicycle);
+
+    System.out.println("【車の情報】");
+    car.printData();
+    System.out.println("-----------------");
+    System.out.println("【車の所有者の情報】");
+    car.getOwner().printData();
+
+    System.out.println("=================");
+    System.out.println("【自転車の情報】");
+    bicycle.printData();
+    System.out.println("-----------------");
+    System.out.println("【自転車の所有者の情報】");
+    bicycle.getOwner().printData();
   }
 }
