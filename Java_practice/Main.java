b06e21013a167ba9b7d9cd9444c35822b417675f
aya-shiro <whitecheck00@gmail.com>
2023-05-24 08:06:53 +0000

    [update]+

--- Java_practice/Main.java
+++ Java_practice/Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
-    Element element1 = new Element(1, "Railsmachine", "about_rails", 20.0, 15.0);
-    System.out.println(element1.createId);
-    element1.printData();
+    Element element = new Element(1, "Railsmachine", "about_rails", 20.0, 15.0);
+    System.out.println(element.createId);
+    element.printData();
   }
 }
