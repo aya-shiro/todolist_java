94b171d2ff19045fa9e0e40d82083cc8c31c7d50
aya-shiro <whitecheck00@gmail.com>
2023-05-31 11:22:38 +0000

    [update]todo_java

--- todo_java/Main.java
+++ todo_java/Main.java
@@ -1,22 +1,31 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
-    TodoList list = new TodoList("タイトル", 27);
     System.out.println("現在のTodoリスト");
-    list.printData();
-    System.out.println("----------------");
+    TodoList todo = null;
+
     System.out.print("追加するTodo：");
     String title = scanner.next();
     System.out.print(title + "の期限：");
     int limit = scanner.nextInt();
 
-    System.out.println(title + "を追加しました。期限は" + limit + "までです");
+    // titleがis.Empty[ではない"!"] = タイトルが入力され、かつlimitがあるとき
+    if (!title.isEmpty() && limit > 0) {
+      todo = new TodoList(title, limit);
+      System.out.println(title + "を追加しました。期限は" + limit + "までです");
+    } else {
+      System.out.println("入力が無効です。Todoの追加はスキップされます。");
+    }
+
+    System.out.println("----------------");
 
     System.out.println("更新後のTodoリスト");
-    list.printData();
+    if (todo != null) {
+      todo.printData();
+    } else {
+      System.out.println("Todoリストは更新されていません");
+    }
   }
 }
